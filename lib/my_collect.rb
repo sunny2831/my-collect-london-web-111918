104f7880e498804724b3f8cd2458097d8e177cf6
Sunny Patel <sunny2831@hotmail.co.uk>
2018-11-10 11:56:20 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+collection = ['ruby', 'javascript', 'python', 'objective-c']
 
+def my_collect(array)
+  i = 0
+  empty_collection = []
+  while i < array.length
+    empty_collection.push yield(array[i])
+    i += 1
+  end
+  empty_collection
+end
 
+my_collect(collection) {|lang|
+  lang.upcase}
